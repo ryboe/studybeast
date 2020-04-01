23955778714467aae4f74090050c8997d28d1e55
Ryan Boehning <ryanboehning@gmail.com>
2020-04-01 00:12:40 -0700

    Initial commit
    This just creates a VPC.

---
+++ terraform/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = "~> 0.12.24"
+  required_providers {
+    tfe         = "~> 0.15.0"
+    google-beta = "~> 3.15.0"
+  }
+  backend "remote" {}
+}
+
+provider "google-beta" {
+  project = var.project_name
+  region  = var.default_region
+  zone    = var.default_zone
+}
+
+resource "google_compute_network" "sg_vpc" {
+  name                    = "sg_vpc"
+  description             = "The studygoose prod VPC."
+  routing_mode            = "GLOBAL"
+  auto_create_subnetworks = true
+}
+
+variable "project_name" {
+  default     = "studygoose-prototype"
+  description = "The main GCP project name."
+  type        = string
+}
+
+variable "default_region" {
+  default     = "us-central1"
+  description = "The default GCP region (us-central1 is in Iowa) for creating resources."
+  type        = string
+}
+
+variable "default_zone" {
+  default     = "us-central1-b"
+  description = "The default GCP zone for creating zonal resources, like VMs."
+  type        = string
+}
