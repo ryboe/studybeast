0c629e8b46c60a2d610d5a666d7363c5d93276cf
Ryan Boehning <ryanboehning@gmail.com>
2020-04-12 03:59:41 -0700

    Comment out development/ terraform files for now

--- terraform/development/variables.tf
+++ terraform/development/variables.tf
@@ -1,10 +1,10 @@
 
-variable "api_db_password" {
-  description = "The db password used by the API service"
-  type        = string
-}
+# variable "api_db_password" {
+#   description = "The db password used by the API service"
+#   type        = string
+# }
 
-variable "cloud_sql_proxy_service_account_key" {
-  description = "The full JSON of the cloud-sql-proxy service account key"
-  type        = string
-}
+# variable "cloud_sql_proxy_service_account_key" {
+#   description = "The full JSON of the cloud-sql-proxy service account key"
+#   type        = string
+# }
