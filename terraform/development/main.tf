d7f8847c6488d83fdd48ded8801dd0f72ae19fbc
Ryan Boehning <ryanboehning@gmail.com>
2020-04-15 23:42:18 -0700

    Update google provider 3.16.0 -> 3.17.0

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -2,8 +2,8 @@
 #   required_version = "~> 0.12.24"
 #   required_providers {
 #     tfe         = "~> 0.15.1"
-#     google      = "~> 3.16.0"
-#     google-beta = "~> 3.16.0"
+#     google      = "~> 3.17.0"
+#     google-beta = "~> 3.17.0"
 #     random      = "~> 2.2"
 #   }
 #   backend "remote" {}
