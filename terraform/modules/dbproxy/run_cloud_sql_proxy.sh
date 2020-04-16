9db77069a6ff4b5e3076c46bcb688a2cf26ff7bb
Ryan Boehning <ryanboehning@gmail.com>
2020-04-16 00:56:22 -0700

    Make startup script work?

--- terraform/modules/dbproxy/run_cloud_sql_proxy.sh
+++ terraform/modules/dbproxy/run_cloud_sql_proxy.sh
@@ -14,17 +14,17 @@ set -euxo pipefail
 #     exit 1
 # fi
 
-echo "${service_account_key}" >/key.json
-chmod 444 key.json
+echo "${service_account_key}" >/tmp/svc_account_key.json
+chmod 444 /tmp/svc_account_key.json
 
-# -p 127.0.0.1:5432:3306    -- cloud_sql_proxy exposes port 3306 on the container, even for Postgres.
-#                              We map 3306 in the container to 5432 on the host. '127.0.0.1' means
-#                              that you can only connect to host port 5432 over localhost.
-# -v /key.json:/key.json:ro -- The file provisioner will copy the service account key file to /key.json
-#                              on the host. We will mount it read-only into the container at the
-#                              same path.
-# -ip_address_types=PRIVATE -- The proxy should only try to connect to the db's private IP.
+# -p 127.0.0.1:5432:3306       -- cloud_sql_proxy exposes port 3306 on the container, even for Postgres.
+#                                 We map 3306 in the container to 5432 on the host. '127.0.0.1' means
+#                                 that you can only connect to host port 5432 over localhost.
+# -v /tmpkey.json:/key.json:ro -- The file provisioner will copy the service account key file to /key.json
+#                                 on the host. We will mount it read-only into the container at the
+#                                 same path.
+# -ip_address_types=PRIVATE    -- The proxy should only try to connect to the db's private IP.
 # -instances=${db_instance_name}=tcp:0.0.0.0:3306 -- The instance name will be something like 'my-project:us-central1:my-db'.
 #                                                    The proxy should accept incoming TCP connections on port 3306.
 docker pull gcr.io/cloudsql-docker/gce-proxy:latest
-docker run --rm -p 127.0.0.1:5432:3306 -v key.json:/key.json:ro gcr.io/cloudsql-docker/gce-proxy:latest /cloud_sql_proxy -credential_file=/key.json -ip_address_types=PRIVATE -instances=${db_instance_name}=tcp:0.0.0.0:3306
+docker run --rm -p 127.0.0.1:5432:3306 -v /tmp/svc_account_key.json:/key.json:ro gcr.io/cloudsql-docker/gce-proxy:latest /cloud_sql_proxy -credential_file=/key.json -ip_address_types=PRIVATE -instances=${db_instance_name}=tcp:0.0.0.0:3306
