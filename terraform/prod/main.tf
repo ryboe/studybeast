fb8c7e21c32b1105d756a8c948b3c28a4145b706
Ryan Boehning <ryanboehning@gmail.com>
2020-04-19 02:42:59 -0700

    Bump version of tfe provider v0.15.1 -> v0.16.0

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = "~> 0.12.24"
   required_providers {
-    tfe         = "~> 0.15.1"
+    tfe         = "~> 0.16.0"
     google      = "~> 3.17.0"
     google-beta = "~> 3.17.0"
     random      = "~> 2.2.1"
