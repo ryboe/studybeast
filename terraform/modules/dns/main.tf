803b86e0884f2d81e2c822f50d300642299efdc9
Ryan Boehning <ryanboehning@gmail.com>
2020-05-01 19:58:26 -0700

    Remove manually specifying nameservers cuz I don't think I need to

--- terraform/modules/dns/main.tf
+++ terraform/modules/dns/main.tf
@@ -14,16 +14,3 @@ resource "google_dns_record_set" "domain_ownership_verification" {
   ttl          = 86400 # 24 hours
   rrdatas      = ["${var.domain_ownership_verification_token}"]
 }
-
-resource "google_dns_record_set" "nameservers" {
-  name         = google_dns_managed_zone.public.dns_name
-  managed_zone = google_dns_managed_zone.public.name
-  type         = "NS"
-  ttl          = 21600 # 6 hours
-  rrdatas = [
-    "ns-cloud-c1.googledomains.com.",
-    "ns-cloud-c2.googledomains.com.",
-    "ns-cloud-c3.googledomains.com.",
-    "ns-cloud-c4.googledomains.com.",
-  ]
-}
