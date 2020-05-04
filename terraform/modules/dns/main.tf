67319fb094eef42b3103d9d9b346043becd7bf79
Ryan Boehning <ryanboehning@gmail.com>
2020-05-04 12:57:42 -0700

    Remove SOA and NS records. They don't work

--- terraform/modules/dns/main.tf
+++ terraform/modules/dns/main.tf
@@ -14,24 +14,3 @@ resource "google_dns_record_set" "domain_ownership_verification" {
   ttl          = 86400 # 24 hours
   rrdatas      = ["${var.domain_ownership_verification_token}"]
 }
-
-resource "google_dns_record_set" "soa" {
-  name         = google_dns_managed_zone.public.dns_name
-  managed_zone = google_dns_managed_zone.public.name
-  type         = "SOA"
-  ttl          = 21600 # 6 hours
-  rrdatas      = ["ns-cloud-c1.googledomains.com. cloud-dns-hostmaster.google.com. 1 21600 3600 259200 300"]
-}
-
-resource "google_dns_record_set" "ns" {
-  name         = google_dns_managed_zone.public.dns_name
-  managed_zone = google_dns_managed_zone.public.name
-  type         = "NS"
-  ttl          = 21600 # 6 hours
-  rrdatas = [
-    "ns-cloud-c1.googledomains.com.",
-    "ns-cloud-c2.googledomains.com.",
-    "ns-cloud-c3.googledomains.com.",
-    "ns-cloud-c4.googledomains.com.",
-  ]
-}
